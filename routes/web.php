797500e01aff26fac3d2e335b33d90b10a51796a
Ibrahima Ciss <bionik6@gmail.com>
2026-03-24 08:23:39 +0000

    Update routes url

--- routes/web.php
+++ routes/web.php
@@ -26,10 +26,10 @@ Route::middleware(['auth', 'verified.phone', 'profile:accountant_firm'])->prefix
 
 Route::middleware(['auth', 'verified.phone', 'profile:sme'])->prefix('pme')->group(function () {
     Route::livewire('dashboard', 'pages::pme.dashboard.index')->name('pme.dashboard');
-    Route::livewire('factures', 'pages::pme.invoices.index')->name('pme.invoices.index');
+    Route::livewire('invoices', 'pages::pme.invoices.index')->name('pme.invoices.index');
     Route::livewire('clients', 'pages::pme.clients.index')->name('pme.clients.index');
-    Route::livewire('recouvrement', 'pages::pme.collection.index')->name('pme.collection.index');
-    Route::livewire('tresorerie', 'pages::pme.treasury.index')->name('pme.treasury.index');
+    Route::livewire('collections', 'pages::pme.collection.index')->name('pme.collection.index');
+    Route::livewire('treasury', 'pages::pme.treasury.index')->name('pme.treasury.index');
     Route::livewire('support', 'pages::pme.support.index')->name('pme.support.index');
 });
 
